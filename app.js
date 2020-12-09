be6ff56ccff37bc75c1f509605acdc7b904c0288
bhara <bharathJ222000@gmail.com>
2020-12-09 18:31:13 +0530

    added

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const shortUrl = require('./models/shortUrl');
 const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017/urlShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log('Error Occured'.red.bold);
+    } else {
+        console.log('Server connected to mongoDB'.cyan.bold);
+    }
+})
+
+
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
